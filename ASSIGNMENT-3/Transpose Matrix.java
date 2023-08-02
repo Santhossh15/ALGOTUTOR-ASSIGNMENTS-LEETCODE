2e47c1363a04088e2a2112304eb3fa0f94814133
Santhossh15 <santhossh15@gmail.com>
2023-08-02 10:14:22 +0530

    new commit

---
+++ ASSIGNMENT-3/Transpose Matrix.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public int[][] transpose(int[][] A)
+    {
+        int M = A.length;
+        int N = A[0].length;
+        int[][] B = new int[N][M];
+        for (int j = 0; j < N; j++)
+        {
+            for (int i = 0; i < M; i++)
+            {
+                 B[j][i] = A[i][j];
+            }
+        }
+        return B;
+    }
+}
